8e65e559f827836b69ae67d02e2d93185daae5e9
IacheLopes <iachelopes1@gmail.com>
2024-10-11 22:05:59 -0300

    deletado o ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Proverbios from "./components/Proverbios";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import AdBanner from "./components/AdBanner";
 
 function App(){
 
   return(
     <>
       <Navbar></Navbar>
-      <AdBanner></AdBanner>
       <Proverbios></Proverbios>
       <Footer></Footer>
     </>
